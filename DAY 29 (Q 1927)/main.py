37bbd21bc2228ad83fa09b09f0e488af6f59ffaf
Namit Singh <namit000777@gmail.com>
2026-08-23 23:52:02 +0530

    Add sumGame method to Solution class
    Implement sumGame method to evaluate game scores.

---
+++ DAY 29 (Q 1927)/main.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def sumGame(self, num):
+
+        n = len(num)
+        half = n // 2
+        
+        sum1 = sum(int(c) for c in num[:half] if c != '?')
+        sum2 = sum(int(c) for c in num[half:] if c != '?')
+        
+        q1 = num[:half].count('?')
+        q2 = num[half:].count('?')
+        
+        if (q1 + q2) % 2 != 0:
+            return True
+        
+        return (sum1 - sum2) * 2 != (q2 - q1) * 9                
