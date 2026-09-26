39a865d3f49246184c6f8b17b85e39307154c8f8
Namit Singh <namit000777@gmail.com>
2026-09-27 02:01:43 +0530

    Simplify string evaluation logic in evaluate method
    Refactor evaluate method for clarity and efficiency.

--- DAY 62(Q 1807)/main.py
+++ DAY 62(Q 1807)/main.py
@@ -1,38 +1,34 @@
 class Solution:
     def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
         
-        i=0
         d={}
-
         for key,val in knowledge:
             d[key]=val
-    
-        n=len(s)
+            
         string=""
         result=""
+        cas=0
 
-        while(i!=n):
-            if(s[i]=="("):
-                i+=1
+        for i in range(len(s)):
+            if (s[i]=="("):
+                cas=1
 
-                while(s[i]!=")"):
-                    string+=s[i]
-                    i+=1
-                i+=1
-
+            elif(s[i]==")"):
+                cas=0
                 if string in d:
                     result+=d[string]
                 else:
                     result+="?"
-
                 string=""
 
-            else:
-                while(i<n and s[i]!="("):
-                    result+=s[i]
-                    i+=1
+            elif(cas==1):
+                string+=s[i]
+            
+            elif(cas==0):
+                result+=s[i]
 
         return result
+
 
         
 
